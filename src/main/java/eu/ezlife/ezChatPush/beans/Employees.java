2d35d5bda2095d83bfe2e380ace33099c9e7a567
Philipp Minges <fjnalta@gmail.com>
2017-04-04 23:23:20 +0200

    initial commit

---
+++ src/main/java/eu/ezlife/ezChatPush/beans/Employees.java
@@ -0,0 +1,15 @@
+package eu.ezlife.ezChatPush.beans;
+
+import java.util.List;
+
+public class Employees {
+	private List<Employee> employeeList;
+
+	public List<Employee> getEmployeeList() {
+		return employeeList;
+	}
+
+	public void setEmployeeList(List<Employee> employeeList) {
+		this.employeeList = employeeList;
+	}
+}
