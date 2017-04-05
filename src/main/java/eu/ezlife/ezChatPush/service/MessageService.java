78acd2ac98115a8956aceba5a29e7fde942d06c7
Philipp Minges <fjnalta@gmail.com>
2017-04-05 16:59:26 +0200

    implement message service

--- src/main/java/eu/ezlife/ezChatPush/service/MessageService.java
+++ src/main/java/eu/ezlife/ezChatPush/service/MessageService.java
@@ -1,6 +1,7 @@
-/*package eu.ezlife.ezChatPush.service;
+package eu.ezlife.ezChatPush.service;
 
-import eu.ezlife.ezChatPush.beans.Token;
+import eu.ezlife.ezChatPush.beans.PushMessage;
+import eu.ezlife.ezChatPush.database.DBService;
 
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.Consumes;
@@ -9,33 +10,37 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.net.URI;
 import java.net.URISyntaxException;
 
-*//**
+/**
  * Created by ajo on 05.04.17.
- *//*
+ */
 @Path("/msg")
 public class MessageService {
+
+    private static DBService dbHandler = new DBService();
 
     @RolesAllowed("ADMIN")
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response sendMessage(Token token, String contactName) throws URISyntaxException {
-        if(token.getUsername() == null || token.getToken() == null) {
+    public Response sendMessage(PushMessage msg) throws URISyntaxException {
+        // Check if the User told the right username + token
+        if(dbHandler.getUserToken(msg.getUserName()).getToken().equals(msg.getToken())) {
+            // Check if the contact got a Token
+            if(!dbHandler.getUserToken(msg.getContactName()).getToken().equals("")) {
+
+                // TODO - connect to GCM and send MSG
+                System.out.println("From: " + msg.getUserName());
+                System.out.println("Token: " + msg.getToken());
+                System.out.println("To (Token): " + dbHandler.getUserToken(msg.getContactName()).getToken());
+            }
+
+            return Response.status(200).entity(msg.toString()).build();
+
+        } else {
+
             return Response.status(400).entity("Token incomplete").build();
         }
-        if(contactName == "") {
-            return Response.status(400).entity("ContactName incomplete").build();
-        }
-
-        // TODO - Check local database for username and token
-
-        // TODO - search for token of contactName
-
-        // TODO - connect to GCM and send MSG
-
-        return Response.created(new URI("/rest/token/"+token.getId())).build();
     }
-}*/
+}
