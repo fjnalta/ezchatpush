4358c140650d5b9cef9257eb6e5d5d1f3e7380f6
Philipp Minges <fjnalta@gmail.com>
2017-04-05 13:42:00 +0200

    implement AppID Service

--- src/main/java/eu/ezlife/ezChatPush/beans/AppID.java
+++ src/main/java/eu/ezlife/ezChatPush/beans/AppID.java
@@ -4,4 +4,34 @@ package eu.ezlife.ezChatPush.beans;
  * Created by ajo on 05.04.17.
  */
 public class AppID {
+    private Integer id;
+    private String appId;
+
+    public AppID() {}
+
+    public AppID(Integer id, String appId) {
+        this.id  = id;
+        this.appId = appId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAppId() {
+        return appId;
+    }
+
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
+
+    @Override
+    public String toString() {
+        return "AppID [id=" + id + ", AppID=" + appId + "]";
+    }
 }
--- src/main/java/eu/ezlife/ezChatPush/service/AppIDService.java
+++ src/main/java/eu/ezlife/ezChatPush/service/AppIDService.java
@@ -1,7 +1,36 @@
 package eu.ezlife.ezChatPush.service;
 
+import eu.ezlife.ezChatPush.beans.AppID;
+import eu.ezlife.ezChatPush.database.DBService;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 /**
  * Created by ajo on 05.04.17.
  */
+
+@Path("/appid")
 public class AppIDService {
+
+    private DBService dbHandler;
+    public AppIDService() {
+
+        dbHandler = new DBService();
+    }
+
+    @RolesAllowed("ADMIN")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public AppID getAppID()
+    {
+        AppID appID = dbHandler.getAppID();
+        System.out.println(appID.toString());
+        return appID;
+    }
 }
