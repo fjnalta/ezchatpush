50a4711d6af22873551431cd6dc8f2a32dd8a4f4
Philipp Minges <fjnalta@gmail.com>
2017-04-05 13:40:27 +0200

    cleanup DBService

--- src/main/java/eu/ezlife/ezChatPush/database/DBService.java
+++ src/main/java/eu/ezlife/ezChatPush/database/DBService.java
@@ -4,62 +4,121 @@ package eu.ezlife.ezChatPush.database;
  * Created by ajo on 05.04.17.
  */
 
-//STEP 1. Import required packages
+import eu.ezlife.ezChatPush.beans.AppID;
+
 import java.sql.*;
 
 public class DBService {
 
     // JDBC driver name and database URL
-    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://localhost/";
+    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://localhost/ezchat";
 
     //  Database credentials
-    static final String USER = "root";
-    static final String PASS = "rootpw";
+    private static final String USER = "ezchat";
+    private static final String PASS = "ezchatpw";
 
-    public static void main(String[] args) {
-        Connection conn = null;
-        Statement stmt = null;
-        try{
+    // Database Tables
+    private static final String TABLE_TOKENS = "tokens";
+    private static final String TABLE_APPID = "appid";
 
-            //STEP 2: Register JDBC driver
-            Class.forName("com.mysql.jdbc.Driver");
+    // Table Columns
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_USERNAME = "username";
+    private static final String COLUMN_RESOURCE = "resource";
+    private static final String COLUMN_TOKEN = "token";
+    private static final String COLUMN_APPID = "appid";
 
-            //STEP 3: Open a connection
-            System.out.println("Connecting to database...");
+    // Database connection
+    private static Connection conn = null;
+
+    public DBService() {
+        connectDatabase();
+        initializeDatabase();
+        disconnectDatabase();
+    }
+
+    public void connectDatabase() {
+        try {
+            // Register JDBC driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // Open a connection
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
-
-            //STEP 4: Execute a query
-            System.out.println("Creating database...");
-            stmt = conn.createStatement();
-
-            String sql = "CREATE DATABASE STUDENTS";
-            stmt.executeUpdate(sql);
-            System.out.println("Database created successfully...");
-
         } catch(SQLException se) {
             //Handle errors for JDBC
             se.printStackTrace();
+
         } catch(Exception e) {
             //Handle errors for Class.forName
             e.printStackTrace();
-        } finally {
-            //finally block used to close resources
-            try {
-                if(stmt!=null)
-                    stmt.close();
-            } catch(SQLException se2) {
-                // nothing we can do
+        }
+    }
+
+    public void initializeDatabase() {
+        // create Tokens and AppId Table
+        Statement stmt;
+
+        String sql = "CREATE TABLE IF NOT EXISTS " + TABLE_TOKENS
+                + "(" + COLUMN_ID + " INTEGER NOT NULL AUTO_INCREMENT, "
+                + COLUMN_USERNAME + " VARCHAR(255), "
+                + COLUMN_RESOURCE + " VARCHAR(255), "
+                + COLUMN_TOKEN + " VARCHAR(255), "
+                + "PRIMARY KEY (id))";
+
+        String sql2 = "CREATE TABLE IF NOT EXISTS " + TABLE_APPID
+                + "(" + COLUMN_ID + " INTEGER NOT NULL AUTO_INCREMENT, "
+                + COLUMN_APPID + " VARCHAR(255), "
+                + "PRIMARY KEY (id))";
+
+        try {
+            stmt = conn.createStatement();
+            stmt.execute(sql);
+            stmt.execute(sql2);
+            stmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void disconnectDatabase() {
+        // close open connection
+        try {
+            if (conn != null) {
+                conn.close();
             }
-            try {
-                if(conn!=null)
-                    conn.close();
-            } catch(SQLException se) {
-                se.printStackTrace();
-            } //end finally try
-        } //end try
 
-        System.out.println("Goodbye!");
+        } catch (SQLException se) {
+            se.printStackTrace();
+        }
+    }
 
-    } //end main
-} //end JDBCExample
+    public AppID getAppID() {
+        connectDatabase();
+
+        Statement stmt;
+        ResultSet rs;
+        AppID appID = new AppID();
+
+        String sql = "SELECT * FROM " + TABLE_APPID + " LIMIT 1";
+
+        try {
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
+
+            while(rs.next()) {
+                appID.setId(rs.getInt(COLUMN_ID));
+                appID.setAppId(rs.getString(COLUMN_APPID));
+            }
+            rs.close();
+            stmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        disconnectDatabase();
+        return appID;
+    }
+
+}
