db1b549e4fc5e9b8fd720a72225ce42684f00975
Philipp Minges <fjnalta@gmail.com>
2017-04-05 13:40:44 +0200

    change formatting

--- src/main/java/eu/ezlife/ezChatPush/CustomApplication.java
+++ src/main/java/eu/ezlife/ezChatPush/CustomApplication.java
@@ -4,10 +4,9 @@ import org.glassfish.jersey.server.ResourceConfig;
 
 import eu.ezlife.ezChatPush.provider.AuthenticationFilter;
 
-public class CustomApplication extends ResourceConfig 
-{
-	public CustomApplication() 
-	{
+public class CustomApplication extends ResourceConfig {
+
+	public CustomApplication() {
 		packages("eu.ezlife.ezChatPush");
 		register(AuthenticationFilter.class);
 	}
