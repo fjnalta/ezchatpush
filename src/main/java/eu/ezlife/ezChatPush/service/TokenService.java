b74f1afcf37ba072debad66a44d14823e83ca7b7
Philipp Minges <fjnalta@gmail.com>
2017-04-05 15:44:09 +0200

    implement TokenService - addToken

--- src/main/java/eu/ezlife/ezChatPush/service/TokenService.java
+++ src/main/java/eu/ezlife/ezChatPush/service/TokenService.java
@@ -1,7 +1,50 @@
 package eu.ezlife.ezChatPush.service;
 
+import eu.ezlife.ezChatPush.beans.Token;
+import eu.ezlife.ezChatPush.database.DBService;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 /**
  * Created by ajo on 05.04.17.
  */
+@Path("/token")
 public class TokenService {
+
+    private static DBService dbHandler = new DBService();
+
+    @RolesAllowed("ADMIN")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addToken(Token token) throws URISyntaxException {
+        if(token == null || token.getUsername() == null || token.getResource() == null || token.getToken() == null) {
+            return Response.status(400).entity("Token invalid").build();
+        }
+
+        if(dbHandler.getUserToken(token.getUsername()).getId() == null) {
+            dbHandler.setUserToken(token);
+        } else {
+            if(!dbHandler.getUserToken(token.getUsername()).getToken().equals(token.getToken())) {
+                dbHandler.setUserToken(token);
+            } else {
+                return Response.status(400).entity("Token already exists").build();
+            }
+        }
+
+        return Response.status(200).entity(token.toString()).build();
+    }
+
+    @RolesAllowed("ADMIN")
+    @DELETE
+    @Path("/{username}")
+    public Response deleteEmployeeByUsername(@PathParam("username") String username)
+    {
+        return Response.status(202).entity("User deleted successfully").build();
+    }
 }
