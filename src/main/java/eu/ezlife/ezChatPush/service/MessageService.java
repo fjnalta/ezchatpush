9bdc819e0a1eccadb4f570ac621a5048391ec1f6
Philipp Minges <fjnalta@gmail.com>
2017-04-06 23:56:29 +0200

    implement Firebase Cloud Message Upstream

--- src/main/java/eu/ezlife/ezChatPush/service/MessageService.java
+++ src/main/java/eu/ezlife/ezChatPush/service/MessageService.java
@@ -1,7 +1,9 @@
 package eu.ezlife.ezChatPush.service;
 
 import eu.ezlife.ezChatPush.beans.PushMessage;
+import eu.ezlife.ezChatPush.beans.Token;
 import eu.ezlife.ezChatPush.database.DBService;
+import org.json.JSONObject;
 
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.Consumes;
@@ -10,13 +12,22 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.List;
 
 /**
  * Created by ajo on 05.04.17.
  */
 @Path("/msg")
 public class MessageService {
+
+    private static String AUTH_KEY = "";
+    private final static String API_URL = "https://fcm.googleapis.com/fcm/send";
 
     private static DBService dbHandler = new DBService();
 
@@ -25,21 +36,51 @@ public class MessageService {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response sendMessage(PushMessage msg) throws URISyntaxException {
+
+        AUTH_KEY = dbHandler.getAppID().getAppId();
+
         // Check if the User told the right username + token
-        if(dbHandler.getUserToken(msg.getUserName()).getToken().equals(msg.getToken())) {
+        if(dbHandler.tokenExists(msg.getUserName(), msg.getToken())) {
             // Check if the contact got a Token
-            if(!dbHandler.getUserToken(msg.getContactName()).getToken().equals("")) {
+            if(dbHandler.hasToken(msg.getContactName())) {
 
-                // TODO - connect to GCM and send MSG
-                System.out.println("From: " + msg.getUserName());
-                System.out.println("Token: " + msg.getToken());
-                System.out.println("To (Token): " + dbHandler.getUserToken(msg.getContactName()).getToken());
+                // connect to GCM and send MSG
+                try {
+                    URL url = new URL(API_URL);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+                    conn.setUseCaches(false);
+                    conn.setDoInput(true);
+                    conn.setDoOutput(true);
+
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty("Authorization","key="+AUTH_KEY);
+                    conn.setRequestProperty("Content-Type",MediaType.APPLICATION_JSON);
+
+                    List<Token> contactTokens = dbHandler.getUserToken(msg.getContactName());
+                    for (Token curToken : contactTokens) {
+
+                        // create JSON Body
+                        JSONObject wrapper = new JSONObject();
+                        wrapper.put("to",curToken.getToken().trim());
+                        JSONObject content = new JSONObject();
+                        content.put("contact", msg.getUserName());
+                        wrapper.put("data", content);
+                        OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+                        wr.write(wrapper.toString());
+                        wr.flush();
+
+                        conn.getInputStream();
+                    }
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-
-            return Response.status(200).entity(msg.toString()).build();
-
+            return Response.status(200).entity("OK").build();
         } else {
-
             return Response.status(400).entity("Token incomplete").build();
         }
     }
